e3a8c702cd6d949becb168bfff5206455f8a2d4e
linwu-hi <linwu.hi@gmail.com>
2023-07-01 09:31:47 +0800

    feat: update

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -3,15 +3,15 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   {
     text: '现代Javascript高级教程',
-    link: 'http://linwu-hi.gitee.io/coding-time',
+    link: 'https://www.coding-time.cn',
   },
   {
     text: '深入浅出Dart',
-    link: 'http://linwu-hi.gitee.io/coding-time-dart',
+    link: 'https://www.coding-time.cn/dart',
   },
   {
     text: '现代Typescript高级教程',
-    link: 'http://linwu-hi.gitee.io/coding-time-typescript',
+    link: 'https://www.coding-time.cn/typescript',
   },
   {
     text: '谷歌插件小册',
