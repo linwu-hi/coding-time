be0794935088555de2c809501f88f469296338ce
wanggangfeng <wanggangfeng@xunlei.com>
2023-07-09 11:30:01 +0800

    deploy

---
+++ assets/index.html-81203f46.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as n,c,e as a}from"./app-4ff2b8c3.js";const r={};function _(s,l){const e=t("AutoCatalog");return n(),c("div",null,[a(e)])}const d=o(r,[["render",_],["__file","index.html.vue"]]);export{d as default};
