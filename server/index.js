1a399e170d89e72054fa004620935646cefbe034
王港锋 <wanggangfeng@xunlei.com>
2025-03-04 21:03:47 +0800

    feat: update

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// 处理所有未匹配的请求，返回主页
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+}); 
