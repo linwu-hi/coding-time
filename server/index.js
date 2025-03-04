e3ca76903cef74b5e3c3fa7f1771109988f57279
王港锋 <wanggangfeng@xunlei.com>
2025-03-04 21:05:03 +0800

    feat: update

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 
 // 设置静态文件目录
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // 处理所有未匹配的请求，返回主页
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
